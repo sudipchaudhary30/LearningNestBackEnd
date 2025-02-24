1ff767c9d69a0989e8c6b5a0faa56c5230ea8ebf
sudip <chsudip30@gmail.com>
2025-02-24 12:07:02 +0545

    MiddlewareMulter

---
+++ middleware/multerMiddleware.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+// Define __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Create the 'uploads' folder if it doesn't exist
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+// Set up storage for uploaded files using multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir); // Save files in the 'uploads' folder
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname)); // Unique filename
+  },
+});
+
+// Initialize multer with the storage configuration
+const upload = multer({ storage });
+
+export default upload;
